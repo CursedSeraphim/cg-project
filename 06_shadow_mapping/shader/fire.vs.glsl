ab74ed901c159ad521d5e644c1a9f372a08e9cfa
Ephiriel <david@eitelsebner.net>
2017-06-08 17:03:08 +0200

    New FireParticleEffects

--- 06_shadow_mapping/shader/fire.vs.glsl
+++ 06_shadow_mapping/shader/fire.vs.glsl
@@ -1,23 +1,20 @@
 
 attribute vec3 a_position;
 attribute vec4 a_color;
+attribute float a_size;
 
-uniform float v_heat;
-varying   vec4  v_Color;
+varying vec4 v_Color;
 
-uniform mat4 u_veloMat;
 uniform mat4 u_modelView;
 uniform mat4 u_projection;
 
 void main(void) {
 
   //compute vertex position in eye space
-	vec4 eyePosition = u_modelView * u_veloMat * vec4(a_position,1);
+	vec4 eyePosition = u_modelView * vec4(a_position,1);
 
 
-  v_Color       = a_color;
+  v_Color = a_color;
 	gl_Position = u_projection * eyePosition;
-  //gl_Position = eyePosition;
-  //gl_Position  = mvpMatrix * vec4(a_position, 1.0);
-  gl_PointSize = v_heat / abs(length(eyePosition.xyz)/5.0);
+  gl_PointSize = a_size / abs(length(eyePosition.xyz)/5.0);
 }
