fe031430cc93753e8b19d835e4ed472bb08c181e
cursedseraphim <christian.steinparz@gmail.com>
2023-08-19 12:49:37 +0200

    separation into files, loading screen

---
+++ code/scripts.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        // Hide the loading screen
+        document.getElementById('loadingScreen').style.display = 'none';
+    }, 3000); // Hide after 3 seconds for demonstrative purposes
+});
