850e637bf87ae08458ec9b4da03099d5df941aaf
cursedseraphim <christian.steinparz@gmail.com>
2023-08-19 15:05:50 +0200

    TODOs

--- code/scripts.js
+++ code/scripts.js
@@ -6,3 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('loadingScreen').style.display = 'none';
     }, 3000); // Hide after 3 seconds for demonstrative purposes
 });
+
+// TODO further below on the page add a fallen animated with the sprite sheet running away from the cursor
+// TODO click on the fallen to kill it with death animation and sound
+// TODO regular fallen sound with position of the sound relative to scroll position and fallen position
+// TODO reinforcement learning to learn the best way for the fallen to run away from the cursor
